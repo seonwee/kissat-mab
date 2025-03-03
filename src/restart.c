fd62929994dfd6a4bde8800040c39a31b33b91e5
daweiguo <daweigu0@qq.com>
2025-03-03 09:43:57 +0800

    fix bug

--- src/restart.c
+++ src/restart.c
@@ -160,7 +160,7 @@ void restart_mab(kissat * solver){
           solver->mab_reward[2] += lrb_reward * coefficient;
           solver->mab_select[2]++;
         }else{
-          double prob = kissat_pick_double(solver->random);
+          double prob = kissat_pick_double(&solver->random);
           if(prob < 0.8){
             solver->mab_reward[2] += lrb_reward * coefficient;
             solver->mab_select[2]++;
